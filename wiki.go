746f3c99e0f72496ce5f5fef1c4cbc8f9a1e699c
Eric Driussi <ericdriussi@gmail.com>
2022-11-06 14:52:50 +0000

    random stuff

--- wiki.go
+++ wiki.go
@@ -1,43 +1,76 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	config "wiki/src"
 	"wiki/src/page"
 	factory "wiki/src/server"
 )
 
 func main() {
+	if len(os.Args[1:]) == 0 {
+		fmt.Println("Launching server on port 8080")
+		fmt.Println("Go to http://localhost:8080/wiki/view/[WIKI_PAGE_TITLE] to check it out")
+		runServer()
+	}
 	for _, a := range os.Args[1:] {
 		if a == "-r" {
-			fmt.Println("Sample Read/Write:")
-			readWrite()
+			runCliRead()
+		} else if a == "-w" {
+			runCliWrite()
 		} else if a == "-s" {
-			fmt.Println("Launching server")
-			serve()
+			// TODO
+			fmt.Println("Setup a bunch of pages (wikipedia)")
 		} else {
-			fmt.Println("NOPE")
+			fmt.Println("Invalid flag")
 		}
 	}
 }
 
-func readWrite() {
-	pageToWrite := page.Page{Title: "test_page", Body: "This is a sample Page."}
+func runCliRead() {
+	fmt.Println("Write the title of the desired wiki page (no spaces allowed):")
+	var title string
+	fmt.Scanln(&title)
+
+	pageToRead, LoadErr := page.Load(title)
+	if LoadErr != nil {
+		log.Fatal("[ERROR]: Couldn't load requested page")
+	}
+	fmt.Println("Page found:")
+	fmt.Println(string(pageToRead.Body))
+}
+
+func runCliWrite() {
+	fmt.Println("Write a title for the new wiki page (no spaces allowed):")
+	var title string
+	fmt.Scanln(&title)
+
+	fmt.Println("Write the content of the new wiki page:")
+	body := basicReader()
+
+	pageToWrite := page.Page{Title: title, Body: body}
 	saveErr := pageToWrite.Save()
 	if saveErr != nil {
 		log.Fatal("[ERROR]: Couldn't save requested page")
 	}
-	pageToRead, LoadErr := page.Load("test_page")
-	if LoadErr != nil {
-		log.Fatal("[ERROR]: Couldn't load requested page")
-	}
-	fmt.Println(string(pageToRead.Body))
 }
 
-func serve() {
+func basicReader() string {
+	reader := bufio.NewReader(os.Stdin)
+	body, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Invalid input", err)
+		return ""
+	}
+	return strings.TrimSuffix(body, "\n")
+}
+
+func runServer() {
 	http.HandleFunc(config.ViewRoute, factory.GetViewHandler())
 	http.HandleFunc(config.EditRoute, factory.GetEditHandler())
 	http.HandleFunc(config.SaveRoute, factory.GetSaveHandler())
