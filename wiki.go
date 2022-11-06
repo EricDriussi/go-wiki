b0f0eec9ebd6208e5d3b3e558dccc209e9f6f54e
Eric Driussi <ericdriussi@gmail.com>
2022-11-06 12:07:30 +0000

    basic args handling

--- wiki.go
+++ wiki.go
@@ -4,11 +4,26 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"wiki/src/page"
 	"wiki/src/server"
 )
 
 func main() {
+	for _, a := range os.Args[1:] {
+		if a == "-r" {
+			fmt.Println("Sample Read/Write:")
+			readWrite()
+		} else if a == "-s" {
+			fmt.Println("Launching server")
+			serve()
+		} else {
+			fmt.Println("NOPE")
+		}
+	}
+}
+
+func readWrite() {
 	pageToWrite := page.Page{Title: "test_page", Body: "This is a sample Page."}
 	saveErr := pageToWrite.Save()
 	if saveErr != nil {
@@ -19,7 +34,9 @@ func main() {
 		log.Fatal("[ERROR]: Couldn't load requested page")
 	}
 	fmt.Println(string(pageToRead.Body))
+}
 
+func serve() {
 	http.HandleFunc("/wiki/", server.ViewHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
